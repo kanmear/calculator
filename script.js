5c81d2c56a42debfe7509b27da4942ce2d9abcd5
kanmear <kanmear+github@gmail.com>
2022-10-23 21:29:02 +0300

    feat: working plus

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ const lowerDisplay = document.querySelector('.lower-part')
 
 const buttonsContainer = document.querySelector('.buttons')
 
+let activeDisplay = lowerDisplay
+let currentOperation = null
+let prevVal = null
+
 buttonsContainer.addEventListener('click', (e) => {
     if (e.target.tagName != 'BUTTON')
         return
@@ -14,8 +18,7 @@ buttonsContainer.addEventListener('click', (e) => {
             lowerDisplay.innerHTML = lowerDisplay.innerHTML.slice(0, -1)
             break
         case 'AC':
-            lowerDisplay.innerHTML = ''
-            upperDisplay.innerHTML = ''
+            reset()
             break
         case '×':
             break
@@ -24,6 +27,7 @@ buttonsContainer.addEventListener('click', (e) => {
         case '-':
             break
         case '+':
+            operate(add, lowerDisplay.innerHTML)
             break
         case '.':
             break
@@ -32,6 +36,45 @@ buttonsContainer.addEventListener('click', (e) => {
         case 'x<sup>y</sup>':
             break
         default:
-            lowerDisplay.innerHTML += element.innerHTML
+            addNumber(element.innerHTML)
     }
 })
+
+const add = {
+    operate(a, b) {
+        console.log(`adding ${a} and ${b}`)
+        return Number(a) + Number(b)
+    },
+    operatorChar: '+'
+}
+
+function operate(operator, value) {
+    currentOperation = currentOperation == null
+        ? operator.operate
+        : currentOperation
+    upperDisplay.innerHTML += lowerDisplay.innerHTML + ` ${operator.operatorChar} `
+    activeDisplay.innerHTML = ''
+
+    if (prevVal) {
+        console.log(`prevVal: ${prevVal}`)
+        let result = currentOperation(value, prevVal)
+        lowerDisplay.innerHTML = result
+        prevVal = result
+    } else {
+        prevVal = value
+    }
+    // b = b.split(' ').at(-1)
+    currentOperation = null
+}
+
+function addNumber(n) {
+    activeDisplay.innerHTML = activeDisplay.innerHTML == '0'
+        ? n
+        : (activeDisplay.innerHTML + n)
+}
+
+function reset() {
+    lowerDisplay.innerHTML = '0'
+    upperDisplay.innerHTML = ''
+    prevVal = null
+}
